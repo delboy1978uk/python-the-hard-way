a764bf14e7caa8c295f4447e23693504f281d698
Derek Stephen McLean <delboy1978uk@gmail.com>
2015-02-02 13:26:30 +0000

    lessons 16 - 20

--- ex16.py
+++ ex16.py
@@ -11,8 +11,8 @@ raw_input("?");
 print "Opening the file..."
 target = open(filename,'w')
 
-print "Truncating the file. Goodbye!"
-target.truncate()
+#print "Truncating the file. Goodbye!"
+#target.truncate()
 
 line1 = raw_input("line 1: ")
 line2 = raw_input("line 2: ")
